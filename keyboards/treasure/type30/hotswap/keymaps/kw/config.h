69147f8fd2ef332126a8bcdef82d602a787bf19d
kw <siraphob47@gmail.com>
2026-01-22 01:03:46 +0700

    adjust treasure type30 hotswap combo config

---
+++ keyboards/treasure/type30/hotswap/keymaps/kw/config.h
@@ -0,0 +1,21 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* ============================================
+ * CONFIGURATION - Matching working lechiffre setup
+ * ============================================ */
+
+/* COMBO SETTINGS */
+#define COMBO_COUNT 3
+#define COMBO_TERM 30
+
+// Reduce buffer sizes for atmega32u2's limited RAM (1KB vs 16KB)
+#define COMBO_BUFFER_LENGTH 4
+#define COMBO_KEY_BUFFER_LENGTH 8
+
+/* MOD-TAP SETTINGS */
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
--- keyboards/treasure/type30/hotswap/keymaps/kw/rules.mk
+++ keyboards/treasure/type30/hotswap/keymaps/kw/rules.mk
@@ -1,1 +1,2 @@
 COMBO_ENABLE = yes
+LTO_ENABLE = yes  # Reduce firmware size to free up RAM
